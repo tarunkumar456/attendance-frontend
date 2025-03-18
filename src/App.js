4127dbaddfaeb1f4d7ec510385172dc25c3f0222
Sachinkumar3942 <sachinkumar3942.9999@gmail.com>
2025-03-18 21:10:28 +0530

    Second

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import Home from './components/Home';
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import { createContext, useState } from 'react';
 import Login from './components/login/Login';
-
+import Attendance from './Attendance';
 export const usercontext = createContext(null);
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
           <Routes>
             <Route path={'/'} element={<Home />}></Route>
             <Route path={'/login'} element={<Login/>}></Route>
-
+            <Route path='/attendance' element={<Attendance/>}></Route>
           </Routes>
     </BrowserRouter>
   );
